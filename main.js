7b1623f9cb1dbf6644224ad6c6546a0667a04dcd
Toni Munder <mundert@gmail.com>
2015-03-28 13:19:54 -0700

    First commit to GitHub

---
+++ main.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+// 	$('.square1 ').on('click', function() {
+// 		$('.taco').show('src', 'tacotoon.png');
+// 	})
+// });
+
+// function handler( event ) {
+// 	var target = $( event.target );
+// 		if ( target.is( ".square 1" ) ) {
+// 			target.children().toggle();
+// 	}
+// }	
+
+// $( ".row1" ).click( handler ).find( ".row1" ).hide();
+
+function sayHello() {
+	alert('Hello World');
+}
