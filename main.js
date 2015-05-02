1e67d33e3f68e7067a3fe10be75ea2cc6b062bec
Toni Munder <mundert@gmail.com>
2015-05-02 13:54:34 -0700

    Trying to fix jQuery

--- main.js
+++ main.js
@@ -52,8 +52,6 @@ function init() {
     moveCount = 0;                                      // This initializing function is used to clear the old values like turn,
 }                                                       // grid array, panel messages, and the grids for the new game
 
-$(document).ready(function () {
-
 $('#playButton').click(function () {
 
     if (hasWinner === 1) {                              // this click is to initialize the game (if there was a winner, a play again button)
@@ -149,8 +147,6 @@ $('.col').click(function () {
             return;
         }
     }
-});
-
 });
 
 function winnerCheck(n, playerName) {
