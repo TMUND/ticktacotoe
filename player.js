973b4c212542b8e0a7b528bd213e53e0e06b3146
Toni Munder <tonimunder@hotmail.com>
2015-05-26 19:10:06 -0700

    Starting work on the refactor

---
+++ player.js
@@ -0,0 +1,20 @@
+var player1 = function(turn) {
+    var name = '';
+    var tickAnimation = function();
+    var animationStart;
+    var animationStop;
+}
+
+var player2 = function(turn) {
+    var name = '';
+    var tickAnimation = function();
+    var animationStart;
+    var animationStop;
+}
+
+var turn = function() {
+    var pickTurn();
+    var switchTurn();
+    var endGame();
+    var resetBoard();
+}
